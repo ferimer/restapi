eb738296d6d8c0161770873f6e61598bac49f7b0
Fernando Rodriguez Sela <frsela@tid.es>
2015-05-05 13:26:36 +0200

    nit: remove debug comment

--- server/index.js
+++ server/index.js
@@ -122,7 +122,6 @@ _RDL_Server.prototype = {
   },
 
   process: function(req, res, endpointData, params) {
-console.log(JSON.stringify(endpointData));
     if (endpointData.data.cors) {
       for (var header in endpointData.data.cors) {
         res.setHeader(header, endpointData.data.cors[header]);
